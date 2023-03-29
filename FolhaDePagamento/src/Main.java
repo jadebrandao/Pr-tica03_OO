4b78910b98780e88fb9016c319385fe903ae29d7
Jade Brandão <108814737+jadebrandao@users.noreply.github.com>
2023-03-29 18:53:19 -0300

    Delete FolhaDePagamento directory

--- FolhaDePagamento/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Fpadrao f1 = new Fpadrao(123, "João", 1000);
-		System.out.println(f1.toString());
-		
-		Fcomissionado f2 = new Fcomissionado(14, "Maria", 1000, 500, 10);
-		System.out.println(f2.toString());
-	}
