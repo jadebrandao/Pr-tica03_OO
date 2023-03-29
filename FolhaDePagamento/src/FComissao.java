8ff6840f85bec95a76a8f21bc9a8be114fb9b6e4
Jade Brandão <108814737+jadebrandao@users.noreply.github.com>
2023-03-28 21:51:45 -0300

    Update FComissao.java

--- FolhaDePagamento/src/FComissao.java
+++ FolhaDePagamento/src/FComissao.java
@@ -1,34 +1,35 @@
-public class FComissao {
+public class FComissao extends Funcionario {
+private double vendas;
+private double percentual;
 
-    private int percentual;
-    private double vendas;
+public Fcomissionado(int matricula, String nome, double salario, double vendas, double percentual) {
+	super(matricula, nome, salario);
+	this.vendas = vendas;
+	this.percentual = percentual;
+}
 
-    public FComissao(int percentual, double vendas) {
-        this.percentual = percentual;
-        this.vendas = vendas;
-    }
+public double getVendas() {
+	return vendas;
+}
 
-    public int getPercentual() {
-        return percentual;
-    }
+public void setVendas(double vendas) {
+	this.vendas = vendas;
+}
 
-    public void setPercentual(int percentual) {
-        this.percentual = percentual;
-    }
+public double getPercentual() {
+	return percentual;
+}
 
-    public double getVendas() {
-        return vendas;
-    }
+public void setPercentual(double percentual) {
+	this.percentual = percentual;
+}
 
-    public void setVendas() {
-        this.vendas = vendas;
-    }
+public double calcularProventos() {
+	return getSalario() + (this.vendas*this.percentual/100);
+}
 
-    public double CalcularProventos() {
-        double proventos;
-        proventos = percentual*vendas/100;
-
-        return proventos;
-    }
+public String toString() {
+	return "Nome: " + getNome() + " Salario: " + calcularProventos();
+}
 
 }
